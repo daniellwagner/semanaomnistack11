939adab66dbffcd2fa6a4078ef0074efb4707aaf
Daniell <dwlima@stefanini.com>
2020-05-26 13:08:47 -0300

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
-import Header from './Header'
+import React from 'react';
+import './global.css';
+import Routes from './routes';
 
-//JSX (Javascript XML) : HTML dentro do javascript
-
+// JSX (JavaScript XML)
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header> Contador: {counter} </Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Routes/>
   );
 }
 
